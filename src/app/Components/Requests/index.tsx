1745f9da8f8fd965d65d383c1e3aa67ac62052b7
nyeliep <nyeliep>
2023-10-24 13:26:59 +0300

    added changes to requests

--- src/app/Components/Requests/index.tsx
+++ src/app/Components/Requests/index.tsx
@@ -2,7 +2,9 @@
 
 import React, { useState, useEffect } from "react";
 import useGetRequests from "@/app/hooks/useGetRequests";
-import { deleteRequest } from "@/app/utilities/utils";
+import useDeleteRequest from "@/app/hooks/useDeleteRequest";
+import useEditRequests from "@/app/hooks/useEditRequest";
+import ConfirmDeleteModal from "../ConfirmDeleteModal";
 
 type Request = {
   id: number;
@@ -20,18 +22,95 @@ type Request = {
 
 const RecentRequest = () => {
   const { requests } = useGetRequests();
+  const { editRequest } = useEditRequests();
+  const { deleteRequestById, isDeleting, error } = useDeleteRequest();
+  // const [request, setRequests] = useState([]); // Initialize your requests
+
+  const [showDeleteConfirmation, setShowDeleteConfirmation] = useState(false);
+  // const [selectedRequestId, setSelectedRequestId] = useState(null);
+  const [request, setRequests] = useState<Request[]>([]);
   const [processingRequests, setProcessingRequests] = useState<Request[]>([]);
   const [requestStatus, setRequestStatus] = useState<{ [key: number]: string }>(
     {}
   );
+  const [newStatus, setNewStatus] = useState("");
 
-  const handleAccept = (id: number) => {
-    setRequestStatus({ ...requestStatus, [id]: "Processing" });
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [selectedRequestId, setSelectedRequestId] = useState(null);
+
+  const openDeleteConfirmation = (id:any) => {
+    setSelectedRequestId(id);
+    setShowDeleteConfirmation(true);
   };
 
-  const handleDecline = (id: number) => {
-    setRequestStatus({ ...requestStatus, [id]: "Cancelled" });
+  const closeDeleteConfirmation = () => {
+    setSelectedRequestId(null);
+    setShowDeleteConfirmation(false);
   };
+
+  const handleDeleteRequest = (id:any) => {
+    openDeleteConfirmation(id);
+  };
+
+  const handleConfirmDelete = () => {
+    if (selectedRequestId) {
+      deleteRequestById(selectedRequestId)
+        .then(() => {
+          const updatedRequests = requests.filter(
+            (request) => request.id !== selectedRequestId
+          );
+          setRequests(updatedRequests);
+        })
+        .catch((error) => {
+          console.error("Error deleting request:", error);
+        });
+    }
+
+    closeDeleteConfirmation();
+  };
+
+
+  const openEditModal = (id: any) => {
+    setSelectedRequestId(id);
+    setShowEditModal(true);
+  };
+
+  const closeEditModal = () => {
+    setSelectedRequestId(null);
+    setShowEditModal(false);
+  };
+
+  const handleEditRequest = (id: any) => {
+    openEditModal(id);
+  };
+
+  const handleStatusChange = () => {
+    if (selectedRequestId && newStatus) {
+      editRequest(selectedRequestId, newStatus)
+        .then((editedRequest) => {
+          console.log("Request edited:", editedRequest);
+          closeEditModal();
+        })
+        .catch((error) => {
+          console.error("Error editing request:", error);
+        });
+    }
+  };
+
+  const handleSubmitEdit = (id: number) => {
+    if (newStatus) {
+      editRequest(id, newStatus)
+        .then((editedRequest) => {
+          console.log("Request edited:", editedRequest);
+        })
+        .catch((error) => {
+          console.error("Error editing request:", error);
+        });
+    }
+  };
+
+
+
 
   return (
     <div>
@@ -41,11 +120,13 @@ const RecentRequest = () => {
         <table className="w-full">
           <thead>
             <tr className="shadow-xl ">
+            {/* <th className="px-6 py-4 text-left">Id</th> */}
               <th className="px-6 py-4 text-left">Product</th>
               <th className="px-6 py-4 text-left">QTY</th>
+
               <th className="px-6 py-4 text-left">Customer</th>
               <th className="px-6 py-4 text-left">Location</th>
-              {/* <th className='px-4 py-2 text-left'>Status</th> */}
+              <th className="px-4 py-2 text-left">Status</th>
               <th className="px-6 py-4 text-left ">Action</th>
             </tr>
           </thead>
@@ -55,6 +136,7 @@ const RecentRequest = () => {
                 key={request.id}
                 className="border-b border-gray-200 shadow-xl"
               >
+                {/* <td className="px-6 py-2">{request.id}</td> */}
                 <td className="px-4 py-2 flex items-center">
                   <img
                     src="/images/laptop3.jpeg"
@@ -66,37 +148,66 @@ const RecentRequest = () => {
                 <td className="px-6 py-2">{request.quantity}</td>
                 <td className="px-4 py-2">{request.requester_name}</td>
                 <td className="px-4 py-2">{request.location}</td>
+                <td className="px-4 py-2">{request.status}</td>
 
-                <td className="px-4 py-10">
+                <td className="px-4 py-14  flex flex-box">
                   <button
                     type="button"
-                    onClick={() => handleAccept(request.id)}
-                    className={`text-white border bg-green-500 rounded w-40 h-10 mr-[10px] text-center ${
-                      requestStatus[request.id] === "Processing"
-                        ? "opacity-50 cursor-not-allowed"
-                        : ""
-                    }`}
-                    disabled={requestStatus[request.id] === "Processing"}
+                    onClick={() => handleEditRequest(request.id)}
+                    className="text-white border bg-green-500 hover:border-blue-700 border-solid-[1px] px-2 py-1 rounded w-40 h-10 mr-4"
                   >
-                    {requestStatus[request.id] === "Processing"
-                      ? "Processing"
-                      : "Accept"}
+                    Accept Request
                   </button>
 
-                  <button
-                    type="button"
-                    onClick={() => handleDecline(request.id)}
-                    className={`text-red-500 border border-red-400 hover:border-red-700 border-solid-[1px] px-2 py-1 rounded w-40 h-10 ${
-                      requestStatus[request.id] === "Cancelled"
-                        ? "opacity-50 cursor-not-allowed"
-                        : ""
-                    }`}
-                    disabled={requestStatus[request.id] === "Cancelled"}
-                  >
-                    {requestStatus[request.id] === "Cancelled"
-                      ? "Cancelled"
-                      : "Decline"}
-                  </button>
+                  {showEditModal && (
+                    <div className="fixed inset-0 flex items-center justify-center z-50 p-6 m-6">
+                      <div className="bg-white p-4 rounded-lg shadow-md  w-[50vw] h-[30vh] text-center">
+                        <p className="pb-4">
+                          Edit the status for this request:
+                        </p>
+                        <select
+                          value={newStatus}
+                          onChange={(e) => setNewStatus(e.target.value)}
+                        >
+                          <option value="processing">Processing</option>
+                          <option value="completed">Completed</option>
+                          <option value="pending">Pending</option>
+                        </select>
+                        <div className="flex flex-box mt-10 justify-center">
+                          <button
+                            className="text-white border border-green-400 bg-green-500 hover:border-green-700 border-solid-[1px] px-2 py-1 rounded w-40 h-10 mt-2 mr-6"
+                            onClick={handleStatusChange}
+                            // onClick ={handleEditRequest}
+                          >
+                            Save
+                          </button>
+                          <button
+                            className="text-red-500 border border-red-400 hover:border-red-700 border-solid-[1px] px-2 py-1 rounded w-40 h-10 mt-2"
+                            onClick={closeEditModal}
+                          >
+                            Cancel
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  )}
+
+                  <div>
+                    {error && <p>Error: {error}</p>}
+                    {isDeleting && <p>Deleting...</p>}
+                    {showDeleteConfirmation && (
+                  <ConfirmDeleteModal
+                   onRequestConfirm={handleConfirmDelete}
+                   onRequestCancel={closeDeleteConfirmation}
+                   />
+                  )}
+                    <button
+                      className="text-red-500 border border-red-400 hover:border-red-700 border-solid-[1px] px-2 py-1 rounded w-40 h-10 "
+                      onClick={() => handleDeleteRequest(request.id)}
+                    >
+                      Decline Request
+                    </button>
+                  </div>
                 </td>
               </tr>
             ))}
