35151e22ee42aa499ed8fe022fc43c18024c98d0
nyeliep <nyeliep>
2023-10-29 14:35:28 +0300

    added changes to request

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = process.env['BASE_URL'];
-// export const BASE_URL = "https://logicspires-backend-5788b2063c3d.herokuapp.com"
+// export const BASE_URL = process.env['BASE_URL'];
+export const BASE_URL = "https://logicspires-backend-5788b2063c3d.herokuapp.com"
 export const IMAGE_URL = process.env['NEXT_PUBLIC_IMAGE_BASE_URL']
