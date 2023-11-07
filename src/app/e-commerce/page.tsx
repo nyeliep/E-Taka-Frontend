4bca0d77d02702b365978875e468b01a715b4d05
nyeliep <nyeliep>
2023-11-07 11:42:53 +0300

    added changes to requests

--- src/app/e-commerce/page.tsx
+++ src/app/e-commerce/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import EcommerceLayout from '../Components/EcommerceLayout';
-import TopNav from '../Components/TopNav';
+
 import BarChart from '../Components/BarChart';
 import TopCards from '../Components/TopNavCards';
 import OrderPage from '../order/page';
@@ -9,7 +9,6 @@ export default function Dashboard() {
     <>
         <EcommerceLayout>
           <div>
-            <TopNav/>
             <TopCards/>
             <BarChart />
           </div>
